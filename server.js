d99a4fac2b43562a521178bf9337fbf12e924bfe
Chris Klanac <cklanac@users.noreply.github.com>
2017-09-23 09:12:28 -0500

    refactor: install and use cors package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
@@ -26,15 +27,7 @@ const app = express();
 app.use(morgan('common'));
 
 // CORS
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
+app.use(cors());
 
 app.use(passport.initialize());
 passport.use(basicStrategy);
