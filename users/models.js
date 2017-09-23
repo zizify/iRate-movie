b1c8def16683c402d64b2d9fc778b1bf953ad41d
Chris Klanac <cklanac@users.noreply.github.com>
2017-09-23 11:23:28 -0500

    fix: shortcircuit mongoose.models to allow `mocha --watch`
    fixes error: `OverwriteModelError: Cannot overwrite `User` model once compiled.`

--- users/models.js
+++ users/models.js
@@ -35,6 +35,6 @@ UserSchema.statics.hashPassword = function(password) {
   return bcrypt.hash(password, 10);
 };
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.models.User || mongoose.model('User', UserSchema);
 
 module.exports = {User};
